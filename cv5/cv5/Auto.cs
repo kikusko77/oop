c40667f69ede56ab4ccf46657f3ba8ef247f157b
kikusko77 <112938118+kikusko77@users.noreply.github.com>
2023-03-13 17:45:46 +0100

    Add files via upload

---
+++ cv5/cv5/Auto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv5
+{
+    public abstract class Auto
+    {
+        public double VelikostNadrze { get; protected set; }
+        public double StavNadrze { get; set; }
+        public TypPaliva Palivo { get; set; }
+
+        public Auto(double velikostNadrze, TypPaliva palivo, double stavNadrze = 0)
+        {
+            VelikostNadrze = velikostNadrze;
+           
+            Palivo = palivo;
+
+            if (stavNadrze > velikostNadrze)
+            {
+                throw new ArgumentException("stav nadrze nemoze byt vacsi nez velikost nadrze");
+            }
+            StavNadrze = stavNadrze;
+
+        }
+
+        public void Natankuj(TypPaliva typ, double mnozstvi)
+        {
+            if (typ != Palivo)
+            {
+                throw new ArgumentException("Nesprávný typ paliva");
+
+            }
+            else if (mnozstvi + StavNadrze > VelikostNadrze)
+            {
+                throw new ArgumentException("Preplnena nadrz");
+            }
+            else
+            {
+                StavNadrze = StavNadrze + mnozstvi;
+            }
+        }
+    }
+
+    public enum TypPaliva
+    {
+        Benzin,
+        Nafta
+    }
+}
---
+++ cv5/cv5/Osobni.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv5
+{
+    public class Osobni : Auto
+    {
+        public int PrepravovaneOsoby { get;private set; }
+        public int MaxOsob { get;private set; }
+
+        public Osobni(double velikostNadrze, TypPaliva palivo, int maxOsob,double stavNadrze=0) : base(velikostNadrze, palivo,stavNadrze)
+        {
+            MaxOsob = maxOsob;
+            PrepravovaneOsoby = 0;
+        }
+        public void NastavPrepravovaneOsoby(int pocet)
+        {
+            if(pocet > MaxOsob)
+            {
+                throw new ArgumentException($"Toto auto neodvezie viac ako {MaxOsob} osob");
+
+            }
+            PrepravovaneOsoby = pocet;
+        }
+    }
+}
