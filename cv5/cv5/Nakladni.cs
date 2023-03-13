0599d0946f90fe1a636005038b85df3b6f00ce6e
Kuric Christián <Christian.Kuric@gopas.sk>
2023-03-13 17:43:43 +0100

    uprava

---
+++ cv5/cv5/Nakladni.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv5
+{
+    public class Nakladni : Auto
+    {
+        public double MaxNaklad { get; private set; }
+        public double PrepravovanyNaklad { get; private set; }
+
+        public Nakladni(double velikostNadrze, TypPaliva palivo, double maxNaklad,double stavNadrze=0) :base(velikostNadrze, palivo, stavNadrze)
+        {
+            MaxNaklad = maxNaklad;
+            PrepravovanyNaklad = 0;
+        }
+        public void NastavPrepravovanyNaklad(double mnozstvi)
+
+        {
+            if (mnozstvi > MaxNaklad)
+            {
+                throw new ArgumentException($"Toto vozidlo moze prepravit max. {MaxNaklad} kg");
+            }
+            PrepravovanyNaklad += mnozstvi;
+            
+        }
+
+    }
+}
