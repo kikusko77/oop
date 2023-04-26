8450f6b6921d7695004c0003cf0a25b2edf60390
kikusko77 <112938118+kikusko77@users.noreply.github.com>
2023-04-26 17:55:52 +0200

    Add files via upload

---
+++ WebApi/CalcDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi
+{
+    public class CalcDTO
+    {
+        public decimal Operand1 { get; set; }
+        public decimal Operand2 { get; set; }
+        public string Operation { get; set; }
+    }
+}
