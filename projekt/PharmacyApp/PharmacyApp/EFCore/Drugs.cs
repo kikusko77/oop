e2057c5e69f1b3bb9431d4b454d6a1bf901263e0
Kuric Christián <christian.kuric@gmail.com>
2023-04-27 10:14:34 +0200

    created project,datase,models

---
+++ projekt/PharmacyApp/PharmacyApp/EFCore/Drugs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyApp.EFCore
+{
+    public class Drugs
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Brand { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Manufacturer { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+    }
+}
---
+++ projekt/PharmacyApp/PharmacyApp/EFCore/PharmacyDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyApp.EFCore
+{
+    public class PharmacyDbContext : DbContext
+    {
+        public DbSet<Drugs> drugs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ChristianKu\Desktop\OopProjekt\oop\projekt\PharmacyApp\PharmacyApp\DrugDatabase.mdf;Integrated Security=True");
+
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Drugs>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Brand).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.Manufacturer).IsRequired().HasMaxLength(50);
+                entity.Property(e => e.Quantity).IsRequired();
+                entity.Property(e => e.Price).IsRequired();
+            });
+        }
+    }
+}
