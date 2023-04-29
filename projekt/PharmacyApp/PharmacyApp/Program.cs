188ba06c8eda71957fe178d709a83218ea1e84da
Kuric Christián <christian.kuric@gmail.com>
2023-04-29 16:39:29 +0200

    skuska

--- projekt/PharmacyApp/PharmacyApp/Program.cs
+++ projekt/PharmacyApp/PharmacyApp/Program.cs
@@ -4,5 +4,18 @@ using Microsoft.EntityFrameworkCore;
 using (var context = new PharmacyDbContext())
     {
         context.Database.Migrate();
-    }
+    var Drugs = new Drugs
+    {
+        Name = "Aspirin",
+        Brand = "Znacka",
+        Manufacturer = "Bayer",
+        Price = 9.99m,
+        Quantity = 100
+    };
+
+    context.drugs.Add(Drugs);
+    context.SaveChanges();
+
+
+}
 
