1cbbfd202c0e7c038caa5a074ee58f9a05dabad7
MarekKoziak <240933@vut.cz>
2023-04-29 18:09:34 +0200

    mdf file

--- projekt/PharmacyApp/MainWindow.xaml.cs
+++ projekt/PharmacyApp/MainWindow.xaml.cs
@@ -49,4 +49,15 @@ namespace PharmacyApp
 
         }
     }
+
+    public class Drugs
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Manufacturer { get; set; }
+        public string Price { get; set; }
+        public string Quantity { get; set; }
+
+    }
 }
